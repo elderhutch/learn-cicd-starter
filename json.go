ec9b3857535941aece6029cc87d773c27625b2f2
James Allard <elderhutch@outlook.com>
2025-06-11 12:34:36 -0400

    Docker setup complete

--- json.go
+++ json.go
@@ -30,7 +30,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	if _, err := w.Write(dat); err != nil {
-		log.Printf("Error writing response: %s", err)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
